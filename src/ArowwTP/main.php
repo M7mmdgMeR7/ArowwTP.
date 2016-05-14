eb1e5aab7d2cf5f24e32f69533dcf6e101c4bdb2
M7mmdgMeR7 <Mohalaamer@icloud.com>
2016-05-14 22:16:10 +0300

    Update main.php

--- src/ArowwTP/main.php
+++ src/ArowwTP/main.php
@@ -1,39 +1,63 @@
 <?php
 
-namespace ArowwTP 
+namespace arrowtp;
 
-use Pocketmine\plugin\PluginBase
+use pocketmine\event\entity\EntityShootBowEvent;
+use pocketmine\event\entity\ProjectileHitEvent;
+use pocketmine\event\inventory\InventoryPickupArrowEvent;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener
+{
 
-public function onEnable(){
-$this->getserver()->getLogger()->info("[ArowwTP]plugin has Been enabled");
- }
-} 
-public function onShoot (\pocketmine\event\entity\EntityShootBowEvent $event)
-		{
-				$shooter = $event->getEntity();
-				if ($shooter instanceof Player) {
-					$id = $shooter->getID();
-					$this->launch[$id] = (bool) true;
-				}
-		}
-		
-		function onHit (\pocketmine\event\entity\ProjectileHitEvent $event)
-		{
-				$entity = $event->getEntity();
-				$level = $entity->getLevel();
-				if ($entity instanceof Arrow) {
-					$shooter = $entity->shootingEntity;
-					$pos = $entity->getPosition();
-					if ($shooter instanceof Player) {
-						$id = $shooter->getID();
-						$shoot = $this->launch[$id];
-						if ($pos instanceof Position and isset($shoot)) {
-							$level->removeEntity($entity);
-							$event->teleport($pos);
-							unset($this->launch[$id]);
-						}
-					}
-				}
-		}
+	public $arrowSessions = [];
+    public function onEnable()
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN . "ArrowTP has been enabled!");
+        $this->saveDefaultConfig();
+
+    }
+
+    public function onDisable()
+    {
+        $this->getLogger()->info(TextFormat::RED . "ArrowTP has been disabled.");
+    }
+
+    public function onArrowShoot(EntityShootBowEvent $ev)
+    {
+        $e = $ev->getProjectile();
+        $en = $ev->getEntity();
+        $lvlname = $e->getLevel()->getName();
+        if (in_array(strtolower($lvlname), $this->getConfig()->get("worlds"))){
+		    if ($en instanceof Player and $en->hasPermission("arrowtp.tp")) {
+                $this->arrowSessions[$e->getId()] = [$e->getId(), $en->getName()];
+            }
+        }
+	}
+
+    public function onArrowHit(ProjectileHitEvent $ev){
+        $e = $ev->getEntity();
+        $id = $e->getId();
+        if(isset($this->arrowSessions[$id])){
+            $p = $this->getServer()->getPlayer($this->arrowSessions[$id][1]);
+            if($p !== null){
+                $p->teleport($e, $p->getYaw(), $p->getPitch());
+            }
+
+        }
+    }
+
+    public function onArrowPickup(InventoryPickupArrowEvent $ev)
+    {
+        if ($this->getConfig()->get("stopCrashes")) {
+            $ev->setCancelled(true);
+            $ev->getArrow()->kill();
+        }
+    }
+
+}
